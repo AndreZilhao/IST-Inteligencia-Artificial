c4996e6fb15629f8770a1b22b152b42f4e7a04e3
Tiago Teixeira <tiagoteixeira@Tiago-Teixeiras-MacBook-Pro.local>
2015-11-28 16:42:16 +0000

    Definicao de A* e best

--- Tetris/src/tetris.lisp
+++ Tetris/src/tetris.lisp
@@ -374,7 +374,71 @@ novo-estado)
 ;;---------------------------------------------|                  |----------------------------------------------
 ;;---------------------------------------------------------------------------------------------------------------
 
+;(defun procura-pp (problema)
+;	(let ((resultado-final nil)
+;		  (estado1 (problema-estado-inicial problema))
+;		  (accoes1 (accoes (problema-estado-inicial problema))))
+;		(progn (setf resultado-final (resultado estado1 (car accoes1))))
+;	resultado-final))
+
 (defun procura-pp (problema)
-	;lista-accoes
-	)
+
+	(let ((solucao nil)
+	      (novalista '())
+	      (lista-accoes-solucoes '()))
+
+		(defun procura-pp1 (problema lista-accoes-solucoes)
+			(let* ((resultado-recursivo lista-accoes-solucoes)
+				  (estado1 (problema-estado-inicial problema))
+				  (accoes1 (reverse (funcall (problema-accoes problema) estado1)))
+				  (estado-pos nil))
+   				(if (funcall (problema-solucao problema) estado1)
+					(progn
+						(setf solucao T)
+						(setf novalista (reverse resultado-recursivo)))
+					(dolist (accao-actual accoes1) 
+						(if (equal solucao nil)
+							(progn
+								(setf estado-pos (funcall (problema-resultado problema) estado1 accao-actual))
+								(setf (problema-estado-inicial problema) estado-pos)
+								(setf lista-accoes-solucoes (append (list accao-actual) lista-accoes-solucoes))
+								(procura-pp1 problema lista-accoes-solucoes)
+								(setf lista-accoes-solucoes (cdr lista-accoes-solucoes))))))))
+
+	(procura-pp1 problema lista-accoes-solucoes)
+	novalista))
+	
+		 
+;	(let ((resultado-final nil)
+;		  (proximo-estado (resultado (problema-estado-inicial problema-actual) (problema-accoes problema-actual)))
+;		   (lista-accoes (reverse (problema-estado problema-actual))))
+;			lista-accoes))
 ;(load "utils.lisp")
+
+;(defun procura-pp-recursivo (estado)
+;	(let ((resultado-recursivo nil)
+;		  (estado-inicial estado)
+;		  (accao-inicial accao)
+;		  (estado-pos nil)
+;		  (accoes-pos nil))
+
+
+;		())
+
+
+;(setf t1 (cria-tabuleiro))
+;(dotimes (coluna 9) (tabuleiro-preenche! t1 0 (+ coluna 1))(tabuleiro-preenche! t1 1 (+ coluna 1))(tabuleiro-preenche! t1 2 (+ coluna 1)))
+;(setf p1 (make-problema :estado-inicial (make-estado :pontos 0 :tabuleiro t1 :pecas-colocadas ()
+; :pecas-por-colocar '(o o o o o l l t t j j i i i i i)) :solucao #'solucao :accoes #'accoes
+;  :resultado #'resultado :custo-caminho #'(lambda (x) 0)))
+
+(defun procura-A* (problema heuristica)
+	(let ((a (list problema heuristica)))
+		(setf a nil)
+		a))
+
+(defun procura-best* (array-tab pecas-p-colocar)
+	(let ((a (list array-tab pecas-p-colocar)))
+		(setf a nil)
+		a))
+(load "utils.fas")
